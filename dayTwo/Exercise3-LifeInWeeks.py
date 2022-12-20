f3532457b0d2b80171df8c871e00a3d7342fbe64
Erick Villafuerte <erick.m.villa.r@gmail.com>
2022-12-20 15:19:16 -0600

    Exercise Life in Weeks

---
+++ dayTwo/Exercise3-LifeInWeeks.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+age = int(age)
+years_of_life = 90 - age
+days = years_of_life * 365  
+weeks = years_of_life  * 52
+months = years_of_life * 12
+print(f"You have {days} days, and {weeks} weeks, and {months} months left")
+
