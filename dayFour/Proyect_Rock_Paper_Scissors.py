d751b8d6b84ad8566859884239af0bd678579475
slizer98 <erick.m.villa.r@gmail.com>
2023-03-14 17:04:10 -0600

    day 4 finished

---
+++ dayFour/Proyect_Rock_Paper_Scissors.py
@@ -0,0 +1,52 @@
+import random
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+#Write your code below this line 👇
+print("Welcome to the Rock Paper Scissors Game")
+user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors. "))
+game_images = [rock, paper, scissors]
+pc_choice = random.randint(0,2)
+
+if user_choice == pc_choice:
+    print("User choice: ")
+    print(game_images[user_choice])
+    print("PC choice: ")
+    print(game_images[pc_choice])
+    print("It's a draw!")
+elif user_choice == 0 and pc_choice == 2 or user_choice == 1 and pc_choice == 0 or user_choice == 2 and pc_choice == 1:
+  print("User choice: ")
+  print(game_images[user_choice])
+  print("PC choice: ")
+  print(game_images[pc_choice])
+  print("You win!")
+else:
+    print("User choice: ")
+    print(game_images[user_choice])
+    print("PC choice: ")
+    print(game_images[pc_choice])
+    print("You lose!")
---
+++ dayFour/lists.py
@@ -0,0 +1,14 @@
+# List:
+#A list is a collection which is ordered and changeable. Allows duplicate members.
+# Create list
+states_of_america = ['California', 'Texas', 'Florida', 'New York']
+print(states_of_america[1])
+# Change value
+states_of_america[1] = 'Washington'
+print(states_of_america[1])
+# add value
+states_of_america.append('Arizona')
+print(states_of_america)
+#  extend list
+states_of_america.extend(['Alaska', 'Hawaii'])
+print(states_of_america)
